98b7e1e8e21d49b335ebc380b26c1e6b7be7da08
Shubham <rhceshubham015@gmail.com>
2024-07-19 13:38:09 +0530

    Initial Commit

---
+++ CricbuzzTest/CricbuzzTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CricbuzzTestApp.swift
+//  CricbuzzTest
+//
+//  Created by shubham gupta on 19/07/24.
+//
+
+import SwiftUI
+
+@main
+struct CricbuzzTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
